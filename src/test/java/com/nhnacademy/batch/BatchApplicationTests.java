55a176db9fc1ee1e3042167a8506f36550adf24d
hoohsh <hoohsh@daum.net>
2024-12-12 17:55:13 +0900

    fix: 코드 스멜 제거

--- src/test/java/com/nhnacademy/batch/BatchApplicationTests.java
+++ src/test/java/com/nhnacademy/batch/BatchApplicationTests.java
@@ -1,13 +1,22 @@
 package com.nhnacademy.batch;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BatchApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        // 애플리케이션 컨텍스트가 정상적으로 로드되는지 확인
+        assertThat(applicationContext).isNotNull();
     }
 
 }
